a4964f3195fadef0824bc61ae650a9734bebe606
Aleksandr <averianovai@gmail.com>
2026-01-20 16:44:21 +0300

    first commit

---
+++ converter.go
@@ -0,0 +1,115 @@
+package converter
+
+import (
+	"fmt"
+	"io/fs"
+	"mime"
+	"os"
+	"path"
+	"strings"
+
+	sl "github.com/Averianov/cisystemlog"
+)
+
+var workPath string
+
+func check(err error) {
+	if err != nil {
+		sl.L.Warning("%s\n", err.Error())
+	}
+}
+
+func init() {
+	sl.CreateLogs("converter", "./log", 4, 0)
+}
+
+func main() {
+	workPath = "./"
+	sl.L.Info("## ConvertWWW - start path: %v\n", workPath+"go")
+
+	ConvertDirectory(workPath+"img", workPath+"img/out", "svg")
+}
+
+func ConvertDirectory(inPath, outPath, typecheck string) (err error) {
+	inPath = checkPath(inPath)
+	outPath = checkPath(outPath)
+
+	var files []fs.DirEntry
+	if files, err = os.ReadDir(inPath); err == nil {
+		sl.L.Debug("## readDirectory: %s\n", inPath)
+		for _, f := range files {
+			if strings.Contains(f.Name(), typecheck) {
+				fileName := f.Name()
+				ConvertFile(inPath, outPath, fileName)
+			}
+		}
+	}
+	return
+}
+
+func ConvertFile(inPath, outPath, filename string) (err error) {
+	var data []byte
+	var contentType string
+	sl.L.Debug("## ReadFileToByte - fileName: %s\n", inPath+filename)
+	data, contentType, err = readFileToBytes(inPath, filename)
+	sl.L.Debug("contentType: %s", contentType)
+	check(err)
+	if err == nil {
+		err = saveByteInGo(outPath, filename, data)
+		check(err)
+	}
+	return
+}
+
+func saveByteInGo(outPath, fileName string, data []byte) (err error) {
+	var f *os.File
+	path := outPath + strings.ToLower(fileName) + ".go"
+
+	sl.L.Info("## SaveInGo - path: %v\n", path)
+	//sl.L.Info("## SaveInGo - data: %v\n", data)
+
+	f, err = os.Create(path)
+	check(err)
+	defer f.Close()
+
+	_, err = f.WriteString(`package memfd
+
+var ` + strings.ToUpper(strings.Replace(strings.Replace(fileName, ".", "", -1), "-", "", -1)) + ` = []byte{` + convertBytes(data) + `}`)
+
+	return
+}
+
+func convertBytes(data []byte) string {
+	var str strings.Builder
+	for _, b := range data {
+		fmt.Fprintf(&str, "%d,", b)
+	}
+	return str.String()[:str.Len()-1]
+}
+
+func readFileToBytes(inPath, file string) ([]uint8, string, error) {
+	pathfile := inPath + file
+	reqFile, err := os.Open(pathfile)
+	if err != nil {
+		return nil, "", err
+	}
+
+	var fi os.FileInfo
+	fi, err = reqFile.Stat() // read file data
+	contentType := mime.TypeByExtension(path.Ext(pathfile))
+	var bytes = make([]uint8, fi.Size())
+	reqFile.Read(bytes)
+	return bytes, contentType, err
+}
+
+func checkPath(path string) string {
+	if path == "" {
+		path = "./"
+	} else {
+		runes := []rune(path)
+		if runes[len(runes)-1] != '/' {
+			path = path + "/"
+		}
+	}
+	return path
+}
---
+++ converter_test.go
@@ -0,0 +1,41 @@
+package converter
+
+import (
+	"os"
+	"testing"
+
+	sl "github.com/Averianov/cisystemlog"
+)
+
+// TestLogging ###############################################
+func TestLogging(t *testing.T) {
+	sl.CreateLogs("", "", 4, 0)
+	sl.L.Debug("TestLogging start agent\n")
+
+	f, err := os.Create("test.txt")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	_, err = f.Write([]byte("some text"))
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	err = ConvertDirectory("./", "./out", "txt")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	t.Errorf("%s", "check result")
+
+	err = os.Remove("test.txt")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	err = os.Remove("./out/test.txt.go")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+}
